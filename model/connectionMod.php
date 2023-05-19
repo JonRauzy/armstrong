afe35a54d81a9b593bd4b3703fdcc65f1746beb1
JonRauzy <jonathan.rauzy@gmail.com>
2023-05-19 07:06:33 +0200

    je suis la

--- model/connectionMod.php
+++ model/connectionMod.php
@@ -6,37 +6,25 @@ function connectUser(PDO $theDB, string $login, string $pwd) {
     $sqlConnect = "SELECT * FROM user WHERE login_user ='$login'";
 
     try {
-
         $queryConnect = $theDB->query($sqlConnect);
-
     } catch (PDOException $e) {
-
         return $e->getMessage();
-
     }
 
     if ($queryConnect->rowCount() === 0) {
-
         return "Echec de connection ! Recommencez !";
-
     }
 
     $recup = $queryConnect->fetch(PDO::FETCH_ASSOC);
 
     if (password_verify($pwd , $recup['pwd_user'])) {
-
         $_SESSION = $recup;
-
         unset($_SESSION['pwd'], $_SESSION['login']);
-
         $_SESSION['myID'] = session_id();
-
         return true;
 
     } else {
-
-        return "on te connait pas";
-
+        return "Echec de connection ! Recommencez !";
     }
 }
 
