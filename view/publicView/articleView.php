d4c12396e6c4bf7785ef206a43202da40046dea1
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-22 15:28:48 +0200

    un peu de design

--- view/publicView/articleView.php
+++ view/publicView/articleView.php
@@ -12,41 +12,42 @@ include_once '../view/include/header.php';
 ?>
     <main>
         <div class="contitreart">
-    <h2 class="titreart"><?= $article['name_article'] ?></h2>
+            <h2><?= $article['name_article'] ?></h2>
         </div>
     <!-- on a d'office un première photo qu'on va afficher -->
-    <section class="conteneurart">
+        <section class="conteneurart">
 
-    <div class="contimgart1">
-    <img class="imgart1" src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
-    </div>
-        <div class="conteneurpara">
-        <p><?= $paragraphe['0']?></p>   
-        
+            <div class="contimgart">
+                <img src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
+            </div>
 
-        <p><?= $paragraphe['1']?></p>
-        </div>
-    <!-- par contre si on pas d'autres photos doit vérifier si elle existe avant d'essayer de l'afficher sinon ça fait une erreur -->
+            <div class="conteneurpara">
+                <p><?= $paragraphe['0']?></p>   
+                <p><?= $paragraphe['1']?></p>
+            </div>
+            <!-- par contre si on pas d'autres photos doit vérifier si elle existe avant d'essayer de l'afficher sinon ça fait une erreur -->
     
-    <div class="contimgart">
-    <div>
-    <?php if(!empty($imageByArticleID[1]['url'])):?>
-        <img class="imgart" src="<?= $imageByArticleID[1]['url'] ?> " alt="">
-    <?php endif; ?>
-    </div>
-    <div>
-    <?php if(!empty($imageByArticleID[2]['url'])):?>
-        <img class="imgart" src="<?= $imageByArticleID[2]['url'] ?> " alt="">
-    <?php endif; ?>
-    </div>
-    </div>
-<div class="conteneurpara">
-    <?php
-    if(!empty($article['wiki_article'])):?>
-        <p><a href="<?=$article['wiki_article']?>">Cliquez sur ce lien pour accéder à l'article Wikipédia de cet instrument.</a></p> 
-<?php endif;?>
-        </div>
-    </section>
+            <div class="contimgart">
+
+                <div>
+                    <?php if(!empty($imageByArticleID[1]['url'])):?>
+                    <img src="<?= $imageByArticleID[1]['url'] ?> " alt="">
+                    <?php endif; ?>
+                </div>
+                    
+                <div>
+                    <?php if(!empty($imageByArticleID[2]['url'])):?>
+                    <img src="<?= $imageByArticleID[2]['url'] ?> " alt="">
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="conteneurpara">
+                <?php
+                if(!empty($article['wiki_article'])):?>
+                    <p><a href="<?=$article['wiki_article']?>">Cliquez sur ce lien pour accéder à l'article Wikipédia de cet instrument.</a></p> 
+                <?php endif;?>
+            </div>
+        </section>
     </main>
 
 <?php endforeach; ?>
