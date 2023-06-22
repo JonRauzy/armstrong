5d33fad55b41c00a8b9519fbf8f9b2cd8cbc05a5
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-22 15:30:07 +0200

    un peu de design

--- view/publicView/articleView.php
+++ view/publicView/articleView.php
@@ -4,51 +4,47 @@ $title = "articles";
 include_once '../view/include/header.php';
 
 // var_dump($articleById);  
-?>
 
-<?php foreach($articleById as $article): 
+    foreach($articleById as $article): 
     $textArticle = $article['max_description_article']; 
     $paragraphe = breakText($textArticle, strlen($textArticle)/2);
-?>
-    <main>
-        <div class="contitreart">
-            <h2><?= $article['name_article'] ?></h2>
-        </div>
-    <!-- on a d'office un première photo qu'on va afficher -->
-        <section class="conteneurart">
-
-            <div class="contimgart">
-                <img src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
+    ?>
+        <main>
+            <div class="contitreart">
+                <h2><?= $article['name_article'] ?></h2>
             </div>
-
-            <div class="conteneurpara">
-                <p><?= $paragraphe['0']?></p>   
-                <p><?= $paragraphe['1']?></p>
-            </div>
-            <!-- par contre si on pas d'autres photos doit vérifier si elle existe avant d'essayer de l'afficher sinon ça fait une erreur -->
-    
-            <div class="contimgart">
-
-                <div>
-                    <?php if(!empty($imageByArticleID[1]['url'])):?>
-                    <img src="<?= $imageByArticleID[1]['url'] ?> " alt="">
-                    <?php endif; ?>
+            <!-- on a d'office un première photo qu'on va afficher -->
+            <section class="conteneurart">
+                <div class="contimgart">
+                    <img src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
                 </div>
-                    
-                <div>
-                    <?php if(!empty($imageByArticleID[2]['url'])):?>
-                    <img src="<?= $imageByArticleID[2]['url'] ?> " alt="">
-                    <?php endif; ?>
+                <div class="conteneurpara">
+                    <p><?= $paragraphe['0']?></p>   
+                    <p><?= $paragraphe['1']?></p>
                 </div>
-            </div>
-            <div class="conteneurpara">
-                <?php
-                if(!empty($article['wiki_article'])):?>
-                    <p><a href="<?=$article['wiki_article']?>">Cliquez sur ce lien pour accéder à l'article Wikipédia de cet instrument.</a></p> 
-                <?php endif;?>
-            </div>
-        </section>
-    </main>
+                <!-- par contre si on pas d'autres photos doit vérifier si elle existe avant d'essayer de l'afficher sinon ça fait une erreur -->
+        
+                <div class="contimgart">
+                    <div>
+                        <?php if(!empty($imageByArticleID[1]['url'])):?>
+                        <img src="<?= $imageByArticleID[1]['url'] ?> " alt="">
+                        <?php endif; ?>
+                    </div>
+                        
+                    <div>
+                        <?php if(!empty($imageByArticleID[2]['url'])):?>
+                        <img src="<?= $imageByArticleID[2]['url'] ?> " alt="">
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="conteneurpara">
+                    <?php
+                    if(!empty($article['wiki_article'])):?>
+                        <p><a href="<?=$article['wiki_article']?>">Cliquez sur ce lien pour accéder à l'article Wikipédia de cet instrument.</a></p> 
+                    <?php endif;?>
+                </div>
+            </section>
+        </main>
 
-<?php endforeach; ?>
+    <?php endforeach; ?>
 <?php include_once "../view/include/footer.php";?>
