affc962287e9bf83069fc76515db52c9f3ae483d
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-30 07:53:35 +0200

    design

--- view/publicView/articleView.php
+++ view/publicView/articleView.php
@@ -6,13 +6,14 @@ include_once '../view/include/header.php';
 
 foreach($articleById as $article): 
 $textArticle = $article['max_description_article']; 
-$paragraphe = breakText($textArticle, strlen($textArticle)/2);
+$paragraphe = breakText($textArticle, strlen($textArticle)/3);
 ?>
 
+<div class="container-art">
     <div class="titre-art">
         <h2><?= $article['name_article'] ?></h2>
     </div>
-    <div class="container-art">
+    <div class="article">
         <!-- on a d'office un première photo qu'on va afficher -->
         <img class="header-img" src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
         
@@ -25,16 +26,22 @@ $paragraphe = breakText($textArticle, strlen($textArticle)/2);
         </div>
 
         <div class="para-art">
+            <p><?= $paragraphe['1']?></p>
             <?php if(!empty($imageByArticleID[2]['url'])):?>
             <img class="left" src="<?= $imageByArticleID[2]['url'] ?> " alt="">
             <?php endif; ?>
-            <p><?= $paragraphe['1']?></p>
+        </div>
+        <div class="para-art">
+            <p><?=$paragraphe['2']?></p>
         </div>
 
-        <?php
-        if(!empty($article['wiki_article'])):?>
-        <p><a href="<?=$article['wiki_article']?>">Cliquez sur ce lien pour accéder à l'article Wikipédia de cet instrument.</a></p> 
-        <?php endif;?>
+        <div class="wiki">
+            <?php
+            if(!empty($article['wiki_article'])):?>
+            <p><a href="<?=$article['wiki_article']?>">Cliquez sur ce lien pour accéder à l'article Wikipédia de cet instrument.</a></p> 
+            <?php endif;?>
+        </div>
     </div>
+</div>
 <?php endforeach; ?>
 <?php include_once "../view/include/footer.php";?>
