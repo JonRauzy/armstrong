4ec966f0cfc98dc65818ddc7798a7816cd2dc412
JonRauzy <jonathan.rauzy@gmail.com>
2023-07-04 18:19:12 +0200

    design 13

--- view/publicView/articleView.php
+++ view/publicView/articleView.php
@@ -14,21 +14,23 @@ $paragraphe = breakText($textArticle, strlen($textArticle)/3);
         <h2><?= $article['name_article'] ?></h2>
     </div>
     <div class="article">
-        <!-- on a d'office un première photo qu'on va afficher -->
-        <img class="header-img" src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
-        
+        <div class="header-article">
+            <!-- on a d'office un première photo qu'on va afficher -->
+            <img src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
+        </div>
+
         <div class="para-art">
+            <img class="para-img" src="<?= $imageByArticleID[1]['url'] ?> " alt="">
             <p><?= $paragraphe['0']?></p>           
             <!-- par contre si on pas d'autres photos doit vérifier si elle existe avant d'essayer de l'afficher sinon ça fait une erreur -->
             <?php if(!empty($imageByArticleID[1]['url'])):?>
-            <img class="right" src="<?= $imageByArticleID[1]['url'] ?> " alt="">
             <?php endif; ?>
         </div>
 
         <div class="para-art">
+            <img class="para-img" src="<?= $imageByArticleID[2]['url'] ?> " alt="">
             <p><?= $paragraphe['1']?></p>
             <?php if(!empty($imageByArticleID[2]['url'])):?>
-            <img class="left" src="<?= $imageByArticleID[2]['url'] ?> " alt="">
             <?php endif; ?>
         </div>
         <div class="para-art">
